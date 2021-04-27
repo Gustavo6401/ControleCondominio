1a8eb8c059591fdfd3444256805cce0944781979
Gustavo6401 <gsoey2019123@gmail.com>
2021-04-27 16:28:48 -0300

    Perdido

--- ServicoPortaria.Presentation/Form1.cs
+++ ServicoPortaria.Presentation/Form1.cs
@@ -1,21 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Windows.Forms;
 
 namespace ServicoPortaria.Presentation
 {
     public partial class Form1 : Form
-    {
+    {        
         public Form1()
         {
+
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
