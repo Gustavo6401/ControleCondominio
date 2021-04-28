49783314c8570c540727a6d7af7ad017f4bb475d
Gustavo6401 <gsoey2019123@gmail.com>
2021-04-28 08:59:06 -0300

    Ta Lindo :v

---
+++ ServicoPortaria.Domain/Interfaces/ICondominioRepository.cs
@@ -0,0 +1,9 @@
+﻿using ServicoPortaria.Domain.Enttities;
+
+namespace ServicoPortaria.Domain.Interfaces
+{
+    public interface ICondominioRepository : IRepositoryBase<Condominio>
+    {
+        
+    }
+}
---
+++ ServicoPortaria.Infra.Data/Repositories/CondominioRepository.cs
@@ -0,0 +1,10 @@
+﻿using ServicoPortaria.Domain.Enttities;
+using ServicoPortaria.Domain.Interfaces;
+
+namespace ServicoPortaria.Infra.Data.Repositories
+{
+    public class CondominioRepository : RepositoryBase<Condominio>, ICondominioRepository
+    {
+
+    }
+}
